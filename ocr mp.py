6b7fc1a68eee645abec4ed014b84e27098e2eced
Yogadarsa <vmyogadarsa@gmail.com>
2026-01-02 06:44:34 +0530

    yg

--- ocr mp.py
+++ ocr mp.py
@@ -3,14 +3,11 @@ import re
 import pandas as pd
 import tkinter as tk
 from tkinter import ttk
-import numpy as np
 
 LARGE_FONT = ("Verdana", 12)
 NORM_FONT = ("Verdana", 10)
 SMALL_FONT = ("Verdana", 8)
-a=12
-b=12
-
+y="yogadarsa"
 def popupmsg(msg):
     popup = tk.Tk()
     popup.wm_title("!")
